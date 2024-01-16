ea16aea6f89f30c6b631a7851087377c6ccd9f87
Flora <floratokam@gmail.com>
2024-01-16 13:51:44 +0100

    (feat/League) MAJ overView

--- lib/tabs/matches.dart
+++ lib/tabs/matches.dart
@@ -1,110 +1,91 @@
 import 'package:flutter/material.dart';
 
 class matches extends StatelessWidget {
-  final String _id = '1';
-  final String team1Logo = 'xxx';
-  final String team1Name = 'Team 1';
-  final String match = '38';
-  final String win = '29';
-  final String defeat = '6';
-  final String _nul = '3';
-  final String time = '99:26';
-  final String pts = '93';
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
+
+  // Ajout d'une variable pour l'espacement entre la deuxième et la troisième colonne
+  final double spacingBetweenColumns = 16.0;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(32.0),
+      padding: const EdgeInsets.all(16.0),
       child: Card(
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: CustomScrollView(
-            slivers: [
-              SliverAppBar(
-                title: buildHeader(),
-                floating: true,
-                pinned: true,
-                expandedHeight: 50.0, // Hauteur de l'en-tête
-                flexibleSpace: FlexibleSpaceBar(
-                  background: Container(
-                    color: Colors.indigoAccent, // Couleur qui surligne l'en-tête
-                  ),
+        clipBehavior: Clip.hardEdge,
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            // Remplacez les valeurs par les données réelles de votre modèle de match
+            String team1Logo = 'path/to/team1_logo.png';
+            String team1Name = 'Team 1';
+            String matchTime = '12:00 PM';
+            String score1 = '2';
+            String team2Logo = 'path/to/team2_logo.png';
+            String team2Name = 'Team 2';
+            String score2 = '1';
+
+            return Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Row(
+                        children: [
+                          // Image.asset(team1Logo, height: 30, width: 30),
+                          SizedBox(width: 8),
+                          Expanded(
+                            child: Text(
+                              team1Name,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          SizedBox(width: spacingBetweenColumns), // Espacement
+                          Text('$score1'),
+                        ],
+                      ),
+                    ),
+                    ListTile(
+                      title: Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              matchTime, // Affichage du temps dans la deuxième ligne
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Ajout d'un espace entre la deuxième et la troisième colonne
+                    SizedBox(height: spacingBetweenColumns),
+                    ListTile(
+                      title: Row(
+                        children: [
+                          // Image.asset(team2Logo, height: 30, width: 30),
+                          SizedBox(width: spacingBetweenColumns), // Espacement
+                          Expanded(
+                            child: Text(
+                              team2Name,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          SizedBox(width: 8),
+                          Text('$score2'),
+                        ],
+                      ),
+                    ),
+                    Divider(height: 0,), // Séparateur entre les matchs
+                  ],
                 ),
               ),
-              SliverList(
-                delegate: SliverChildBuilderDelegate(
-                      (context, index) {
-                    if (index == 0) {
-                      // L'en-tête a déjà été affichée
-                      return SizedBox.shrink();
-                    } else {
-                      // Les éléments suivants avec séparateurs
-                      return buildRow(index);
-                    }
-                  },
-                  childCount: 25, // nombre total d'éléments suivants
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildHeader() {
-    return Row(
-      children: [
-        buildCell('ID'),
-        buildCell('Logo'),
-        buildCell('Team'),
-        buildCell('Matches'),
-        buildCell('Wins'),
-        buildCell('Defeats'),
-        buildCell('Draws'),
-        buildCell('Time'),
-        buildCell('Points'),
-      ],
-    );
-  }
-
-  Widget buildRow(int index) {
-    return Row(
-      children: [
-        buildCell(_id),
-        buildCell(team1Logo),
-        buildCell(team1Name),
-        buildCell(match),
-        buildCell(win),
-        buildCell(defeat),
-        buildCell(_nul),
-        buildCell(time),
-        buildCell(pts),
-      ],
-    );
-  }
-
-  Widget buildCell(String data) {
-    return Expanded(
-      child: Container(
-        padding: EdgeInsets.all(8.0),
-        decoration: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(color: Colors.grey), // Ligne horizontale entre les cellules
-          ),
-        ),
-        child: Text(
-          data,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 14.0, // Taille du texte de l'en-tête
-            color: Colors.black, // Couleur du texte de l'en-tête
-          ),
+            );
+          },
         ),
       ),
     );
   }
 }
-
-
--- lib/tabs/overView.dart
+++ lib/tabs/overView.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class overView extends StatelessWidget {
-  //const OverviewList({super.key});
   final List<String> entries = <String>['A', 'B', 'C'];
   final List<int> colorCodes = <int>[600, 500, 100];
+
+  // Ajout d'une variable pour l'espacement entre la deuxième et la troisième colonne
+  final double spacingBetweenColumns = 16.0;
 
   @override
   Widget build(BuildContext context) {
@@ -11,20 +13,17 @@ class overView extends StatelessWidget {
       padding: const EdgeInsets.all(16.0),
       child: Card(
         clipBehavior: Clip.hardEdge,
-
         child: ListView.builder(
           itemCount: 10,
           itemBuilder: (context, index) {
-
             // Remplacez les valeurs par les données réelles de votre modèle de match
             String team1Logo = 'path/to/team1_logo.png';
             String team1Name = 'Team 1';
-            String team2Logo = 'path/to/team2_logo.png';
-            String team2Name = 'Team 2';
             String matchTime = '12:00 PM';
             String score1 = '2';
+            String team2Logo = 'path/to/team2_logo.png';
+            String team2Name = 'Team 2';
             String score2 = '1';
-
 
             return Container(
               color: Colors.white,
@@ -33,41 +32,50 @@ class overView extends StatelessWidget {
                 child: Column(
                   children: [
                     ListTile(
-                      title: Expanded(
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                //Image.asset(team1Logo, height: 30, width: 30),
-                                SizedBox(width: 8),
-                                Text(team1Name),
-
-                                SizedBox(width: 241),
-                                Text('$score1'),
-                              ],
+                      title: Row(
+                        children: [
+                          // Image.asset(team1Logo, height: 30, width: 30),
+                          SizedBox(width: 8),
+                          Expanded(
+                            child: Text(
+                              team1Name,
+                              overflow: TextOverflow.ellipsis,
                             ),
-                            Text('$matchTime'),
-                          ],
-                        ),
+                          ),
+                          SizedBox(width: spacingBetweenColumns), // Espacement
+                          Text('$score1'),
+                        ],
                       ),
                     ),
-
                     ListTile(
-                      title: Expanded(
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                //Image.asset(team2Logo, height: 30, width: 30),
-                                SizedBox(width: 8),
-                                Text(team2Name),
-
-                                SizedBox(width: 241),
-                                Text('$score2'),
-                              ],
+                      title: Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              matchTime, // Affichage du temps dans la deuxième ligne
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.center,
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Ajout d'un espace entre la deuxième et la troisième colonne
+                    SizedBox(height: spacingBetweenColumns),
+                    ListTile(
+                      title: Row(
+                        children: [
+                          // Image.asset(team2Logo, height: 30, width: 30),
+                          SizedBox(width: spacingBetweenColumns), // Espacement
+                          Expanded(
+                            child: Text(
+                              team2Name,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          SizedBox(width: 8),
+                          Text('$score2'),
+                        ],
                       ),
                     ),
                     Divider(height: 0,), // Séparateur entre les matchs
@@ -80,5 +88,4 @@ class overView extends StatelessWidget {
       ),
     );
   }
-
 }
